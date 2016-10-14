e201e67e8062d51294a65f4f2ae15cc3d4437c1a
MisterHyde <felix.fischer@hs-augsburg.de>
2016-10-14 12:02:52 +0200

    New folder for scripts

---
+++ scripts/chkeymap.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+
+import subprocess
+
+class Py3status:
+
+    def __init__(self):
+        process = subprocess.Popen(['setxkbmap', '-print'], shell=False, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        out = out.decode('utf-8')
+        for line in out.split("\n"):
+            if "xkb_symbol" in line:
+                self.full_text = "\u2691 %s" %(line.split("+")[1])
+
+    def click_info(self):
+        return {
+                'full_text': self.full_text,
+                'cached_until': self.py3.CACHE_FOREVER
+        }
+
+    def on_click(self, event):
+        button = event['button']
+        keymap = 'de'
+        if "de" in self.full_text:
+            keymap = 'us'
+        if "us" in self.full_text:
+            keymap = 'de'
+        process = subprocess.Popen(['setxkbmap', keymap], shell=False, stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE)
+        out, err = process.communicate()
+        # out = out.decode('utf-8')
+        # out = out.strip("\n")
+        data = {'lang': keymap}
+
+        format_string = "\u2691 {lang}" 
+        self.full_text = self.py3.safe_format(format_string, data)
+        
